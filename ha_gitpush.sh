5eb277ff922b7d89ccaab32645cc289e6e662815
root <root@a0d7b954-vscode.local.hass.io>
2024-09-03 23:11:28 +0300

    config files on 03-09-2024 23:11:28

--- ha_gitpush.sh
+++ ha_gitpush.sh
@@ -2,7 +2,7 @@
 # Change this to your Home Assistant config folder if it is different
 cd /config
 
-git config core.sshCommand 'ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i /config/.ssh/id_rsa -F /dev/null'
+git config core.sshCommand 'ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i /root/.ssh/id_rsa -F /dev/null'
 
 # Add all files to the repository with respect to .gitignore rules
 git add .
